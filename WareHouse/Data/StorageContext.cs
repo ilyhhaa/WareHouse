4541d0b978d1ef0560bb1cfcb770d44a9151cf78
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-09-29 14:56:01 +0300

    Model + DbContext

---
+++ WareHouse/Data/StorageContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WareHouse.Models;
+
+namespace WareHouse.Data
+{
+    public class StorageContext:DbContext
+    {
+        public StorageContext(DbContextOptions<StorageContext> options):base (options) { }
+
+        public DbSet<StorageModel> Storages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    
+}
---
+++ WareHouse/Models/StorageModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WareHouse.Models
+{
+    public class StorageModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Capacity { get; set; }
+        public int AvailableCapacity { get; set; }
+        public string ContactInfo { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
